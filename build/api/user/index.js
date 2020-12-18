2acbfdf662dc25968199adafce2f2bc025a8a189
wuqiao <qiao.wu@pingcl.com>
2020-12-18 15:37:20 +0800

    feat: 去除编译后的文纪检

--- build/api/user/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiGoodsRouter = void 0;
-const express_1 = require("express");
-exports.apiGoodsRouter = express_1.Router();
-//  $router         GET /api/goods
-//  @desc           查询所有信息列表
-// apiGoodsRouter.get('/',"");
--- build/model/shared/goodFilter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GoodFilter = void 0;
-class GoodFilter {
-    constructor(data) {
-        this.currency = data.currency;
-    }
-}
-exports.GoodFilter = GoodFilter;
